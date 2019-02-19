21be7b52e3e459515d125f5cffc8fd1edaa62401
Takarkiz <ponpontanuki16@gmail.com>
2019-02-19 18:28:11 +0900

    add bottom navigationView

--- app/src/main/java/com/test/sawada/fragmentpracti/MainActivity.java
+++ app/src/main/java/com/test/sawada/fragmentpracti/MainActivity.java
@@ -2,8 +2,13 @@ package com.test.sawada.fragmentpracti;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -12,11 +17,36 @@ public class MainActivity extends AppCompatActivity implements AddFragment.OnFra
 
     FragmentManager fragmentManager = getSupportFragmentManager();
     FragmentTransaction transaction;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_home:
+                        Toast.makeText(MainActivity.this, "ホーム", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.navigation_dashboard:
+                        Toast.makeText(MainActivity.this, "ダッシュボード", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.navigation_notifications:
+                        Toast.makeText(MainActivity.this, "通知", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    default:
+                        break;
+                }
+
+                return false;
+            }
+        });
+
         //トランザクションの開始
         transaction = fragmentManager.beginTransaction();
         //トランザクションにfragmentを追加
@@ -51,6 +81,11 @@ public class MainActivity extends AppCompatActivity implements AddFragment.OnFra
         transaction.addToBackStack(null);
         //出力
         transaction.commit();
+
+        fragmentManager.executePendingTransactions();
+
+        Log.d("TAG:DebugLog", String.valueOf(fragmentManager.getBackStackEntryCount()));
+        Log.d("TAG:DebugLog", url);
     }
 
     @Override
@@ -58,6 +93,7 @@ public class MainActivity extends AppCompatActivity implements AddFragment.OnFra
         //バックスタックに保存されていれば戻る
         if (fragmentManager.getBackStackEntryCount() > 0) {
             fragmentManager.popBackStack();
+            Log.d("TAG:DebugLog", String.valueOf(fragmentManager.getBackStackEntryCount()));
             return;
         }
         super.onBackPressed();
